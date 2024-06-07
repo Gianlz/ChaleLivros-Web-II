d05a10133320360967b874137e6ec87b31e54d4e
Gianlz <gianlucazugnoifc@gmail.com>
2024-06-07 02:45:16 -0300

    Added Models and robots

--- LivrariaDjango/Livraria_IFC/myapp/models.py
+++ LivrariaDjango/Livraria_IFC/myapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Database models
+class Livro(models.Model):
+    nome = models.CharField(max_length=100)
+    autor = models.CharField(max_length=100)
+    editora = models.CharField(max_length=100)
+    ano = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.nome  # Display book name in admin panel
+
+class Usuario(models.Model):
+    MASCULINO = 'M'
+    FEMININO = 'F'
+    GENERO_CHOICES = [
+        (MASCULINO, 'Masculino'),
+        (FEMININO, 'Feminino'),
+    ]
+
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    senha = models.CharField(max_length=100)  # Consider using password hashing for security
+    telefone = models.CharField(max_length=15)
+    nascimento = models.DateField()
+    genero = models.CharField(max_length=1, choices=GENERO_CHOICES)
+
+    def __str__(self):
+        return self.nome  # Display user name in admin panel
--- LivrariaDjango/Livraria_IFC/myapp/views.py
+++ LivrariaDjango/Livraria_IFC/myapp/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def robots(request):
+    return render(request, 'robots.txt')
