aebabc184a7ba6be37fd5ed8a3bd9392aac33de9
Gianlz <gianlucazugnoifc@gmail.com>
2024-06-07 02:21:05 -0300

    Updated Models and add some secrets

--- LivrariaDjango/Livraria_IFC/myapp/admin.py
+++ LivrariaDjango/Livraria_IFC/myapp/admin.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 # Exmaple for registering models
-# from .models import Livro
-# admin.site.register(Livro)
+from .models import Livro, Usuario
+
+admin.site.register(Livro)
+admin.site.register(Usuario)
