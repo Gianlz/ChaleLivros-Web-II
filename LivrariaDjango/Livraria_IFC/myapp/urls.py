91fbc05c2edb1055141d2db4d193c08987658d5b
André-Lopes <andredavilopes6@gmail.com>
2024-06-10 10:36:48 -0300

    tela-login

--- LivrariaDjango/Livraria_IFC/myapp/urls.py
+++ LivrariaDjango/Livraria_IFC/myapp/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("home/", views.home, name="home"),
+    path("login/", views.login, name="login"),
     path("robots.txt", views.robots, name="robots"),
 ]
