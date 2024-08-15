f7a54331546c191b28110967a615594fe8f892f2
André-Lopes <andredavilopes6@gmail.com>
2024-08-15 08:03:06 -0300

    removed-user-from-models

--- myapp/models.py
+++ myapp/models.py
@@ -19,33 +19,6 @@ class Livro(models.Model):
         livro_logger.debug(f'Livro salvo: {self.nome}')
         super().save(*args, **kwargs)
 
-
-# Obtém um logger para o modelo Usuario
-usuario_logger = logging.getLogger('usuario_logger')
-
-class Usuario(models.Model):
-    MASCULINO = 'M'
-    FEMININO = 'F'
-    GENERO_CHOICES = [
-        (MASCULINO, 'Masculino'),
-        (FEMININO, 'Feminino'),
-    ]
-
-    nome = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100, unique=True)
-    senha = models.CharField(max_length=100)  # Consider using password hashing for security
-    telefone = models.CharField(max_length=15)
-    nascimento = models.DateField()
-    genero = models.CharField(max_length=1, choices=GENERO_CHOICES)
-
-    def __str__(self):
-        return self.nome
-
-    def save(self, *args, **kwargs):
-        usuario_logger.debug(f'Usuário salvo: {self.nome}')
-        super().save(*args, **kwargs)
-
-
 # Obtém um logger para o modelo GoogleUser
 googleuser_logger = logging.getLogger('googleuser_logger')
 
